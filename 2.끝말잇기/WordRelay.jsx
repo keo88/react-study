db45db607f245271c594bd6d720da499a6283eee
keo88 <keo.kim0808@gmail.com>
2023-04-15 22:13:26 +0900

    feat: word relay in functional component.

--- 2.끝말잇기/WordRelay.jsx
+++ 2.끝말잇기/WordRelay.jsx
@@ -1,12 +1,32 @@
 const React = require('react');
 
 function WordRelay() {
-    const [helloWorldText, setHelloWorldText] = React.useState('Hello World');
+    const [title, setTitle] = React.useState('끝말잇기');
+    const [wordValue, setWordValue] = React.useState('');
+    const inputElem = React.useRef(null);
+
+    function onSubmitForm(e) {
+        e.preventDefault();
+        if (wordValue[0] === title[title.length - 1]) {
+            setTitle(wordValue);
+            setWordValue('');
+        }
+
+        inputElem.current.focus();
+    }
+
+    function onWordInput(e) {
+        setWordValue(e.target.value);
+    }
 
     return (
-        <h1>
-            {helloWorldText}
-        </h1>
+        <>
+            <div>{title}</div>
+            <form onSubmit={onSubmitForm}>
+                <input value={wordValue} ref={inputElem} onChange={onWordInput}/>
+                <button>입력!</button>
+            </form>
+        </>
     );
 }
 
