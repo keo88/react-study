ace781938a4a67a745ac81301c814a98f7b8ebf1
keo88 <keo.kim0808@gmail.com>
2023-04-17 13:27:04 +0900

    fix: use createRoot for react 18+.

--- 2.끝말잇기/client.jsx
+++ 2.끝말잇기/client.jsx
@@ -1,6 +1,6 @@
 const React = require('react');
-const ReactDom = require('react-dom');
+const ReactDom = require('react-dom/client');
 const WordRelay = require('./WordRelay');
 
-// ReactDom.createRoot(document.querySelector('#root')).render(<GuGuDan />);
-ReactDom.render(<WordRelay />, document.querySelector('#root'));
+ReactDom.createRoot(document.querySelector('#root')).render(<WordRelay />);
+// ReactDom.render(<WordRelay />, document.querySelector('#root'));
