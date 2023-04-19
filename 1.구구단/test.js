3bdae4dee537c6f5db5e32e1b57da5e6683d3d13
keo88 <keo.kim0808@gmail.com>
2023-04-19 13:19:02 +0900

    chore: some experiment with test script.

--- 1.구구단/test.js
+++ 1.구구단/test.js
@@ -9,3 +9,18 @@ function fun1 () {
 
     closure2();
 }
+
+console.log(document.querySelector('#track_form').children.namedItem('duration'));
+document.querySelector('#track_form').addEventListener('submit', function (e) {
+    console.log('submit');
+});
+
+getEventListeners(document.querySelector('#send_progress_btn'));
+
+var f1 = temp1[0].listener;
+function f_l (a) {
+    console.log(a);
+    return f1(a);
+}
+
+document.querySelector('#send_progress_btn').addEventListener('click', f_l);
