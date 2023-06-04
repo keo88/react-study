67fb5137696d946f0656c3782e00e382c1dd4c82
keo88 <keo.kim0808@gmail.com>
2023-06-04 14:11:46 +0900

    fix: import react-dom/client instead of react-dom.

--- 3.숫자야구/client.jsx
+++ 3.숫자야구/client.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDom from 'react-dom';
+import ReactDom from 'react-dom/client';
 import NumberBaseBall from './NumberBaseBall';
 
 ReactDom.createRoot(document.querySelector('#root')).render(<NumberBaseBall />);
