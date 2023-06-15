1b88d85779033ba27079e0e5c110efd9e88d4b83
keo88 <keo.kim0808@gmail.com>
2023-06-15 17:27:05 +0900

    feat: wrap TryComponent with memo.

--- 3.숫자야구/Try.jsx
+++ 3.숫자야구/Try.jsx
@@ -1,12 +1,14 @@
-import React, { Component } from 'react';
+import React, { memo } from 'react';
 
-const TryComponent = (props) => {
+const TryComponent = memo((props) => {
     return (
         <>
             <div>{props.index} - {props.value.value}</div>
             <div>스트라이크: {props.value.strikes} 볼: {props.value.balls}</div>
         </>
     );
-}
+});
+
+TryComponent.displayName = 'Try';
 
 export default TryComponent;
