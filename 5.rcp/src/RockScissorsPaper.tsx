137802e92d02b817857b52fd8ccd780842fb910a
keo88 <keo.kim0808@gmail.com>
2023-06-22 00:23:16 +0900

    feat: rock scissors paper display image from web.

--- 5.rcp/src/RockScissorsPaper.tsx
+++ 5.rcp/src/RockScissorsPaper.tsx
@@ -13,14 +13,28 @@ const scores = {
 }
 
 function RockScissorsPaper() {
-  const [imgCoord, setImgCoord] = useState('0');
+  const [imgCoord, setImgCoord] = useState(rspCoords.rock);
 
   const onClickBtn = (s: string) => {
     return undefined;
   };
 
+  const switchComputerRspState = () => {
+    if (imgCoord === rspCoords.rock) {
+      setImgCoord(rspCoords.scissors);
+    } else if (imgCoord === rspCoords.scissors) {
+      setImgCoord(rspCoords.paper);
+    } else if (imgCoord === rspCoords.paper) {
+      setImgCoord(rspCoords.rock);
+    }
+  }
+
   return (<>
-      <div id="computer" style={{background: `url(https://en.pimg.jp/023/182/267/1/23182267.jpg) ${imgCoord} 0`}} />
+      <div id="computer" style={{
+        background: `url(https://en.pimg.jp/023/182/267/1/23182267.jpg) ${imgCoord} 0`,
+        width: '142px',
+        height: '200px',
+      }} />
       <div>
         <button id="rock" className="btn" onClick={onClickBtn('바위')}>바위</button>
         <button id="scissors" className="btn" onClick={onClickBtn('가위')}>가위</button>
