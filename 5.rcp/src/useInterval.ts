931c5962355a48ef33c28c1ec0be1b11c711cef6
keo88 <keo.kim0808@gmail.com>
2023-06-22 16:08:43 +0900

    feat: implement custom hooks for interval update.

---
+++ 5.rcp/src/useInterval.ts
@@ -0,0 +1,25 @@
+import { useRef, useEffect } from 'react';
+
+function useInterval(callback : () => void, delay: number) {
+  const savedCallback = useRef<() => void>();
+
+  useEffect(() => {
+    savedCallback.current = callback;
+  });
+
+  useEffect(() => {
+    function tick() {
+      if (savedCallback.current !== undefined)
+        savedCallback.current();
+    }
+
+    if(delay !== null) {
+      let id = setInterval(tick, delay);
+      return () => clearInterval(id);
+    }
+
+    return savedCallback.current;
+  }, [delay]);
+}
+
+export default useInterval;
