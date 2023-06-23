bd4de3253e7b60a9aa5a03cad53f21b26e7472db
keo88 <keo.kim0808@gmail.com>
2023-06-23 16:36:51 +0900

    feat: import Ball component on Lotto.tsx.

--- 6.reactgames/src/Lotto.tsx
+++ 6.reactgames/src/Lotto.tsx
@@ -1,4 +1,49 @@
-import React from 'react';
+import React, {Component} from 'react';
+import Ball from "./Ball";
+
+interface IState {
+  winNumbers: number[];
+  winBalls: number[];
+  bonus: null | number;
+  redo: boolean;
+}
+
+interface IProps {
+
+}
+
+
+
+export class LottoClass extends Component<IProps, IState> {
+  constructor(props: IProps) {
+    super(props);
+    this.state = {
+      winNumbers: [],
+      winBalls: [],
+      bonus: null,
+      redo: false,
+    }
+  }
+  onClickRedo = () => {
+
+  }
+
+  render() {
+    const {winNumbers, winBalls, bonus, redo} = this.state;
+
+    return (
+      <>
+        <div>당첨 숫자</div>
+        <div id="결과창">
+          {winBalls.map((v) => <Ball key={v} number={v} />)}
+        </div>
+        <div>보너스!</div>
+        {bonus && <Ball number={bonus} />}
+        {redo && <button onClick={this.onClickRedo}>한 번 더!</button>}
+      </>
+    )
+  }
+}
 
 function Lotto() {
 
