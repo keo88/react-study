ebd7da2721c74138ff8b9ae28790421cd2ba0099
keo88 <keo.kim0808@gmail.com>
2023-06-23 16:14:48 +0900

    feat: init react game project.

---
+++ 6.reactgames/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import Lotto from "./Lotto";
+
+function App() {
+  return (
+    <Lotto />
+  );
+}
+
+export default App;
---
+++ 6.reactgames/src/Lotto.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Lotto() {
+
+
+  return (
+    <></>
+  );
+}
+
+export default Lotto;
