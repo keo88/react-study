e7004ed9ce9ca3070f5bfb62e3f1efe50ba0747f
keo88 <keo.kim0808@gmail.com>
2023-07-06 15:48:17 +0900

    feat: add style and text to minefield.

--- 6.reactgames/src/MineTd.tsx
+++ 6.reactgames/src/MineTd.tsx
@@ -1,12 +1,33 @@
 import React from 'react';
-import { CellType } from './MineSearchModel';
+import { CellType, CODE } from './MineSearchModel';
 
 interface IProps {
   tdData: CellType;
 }
 
+const getTdStyle = (tdData: number) => {
+  switch (tdData) {
+    case CODE.NORMAL:
+    case CODE.MINE:
+      return { background: '#444' };
+    default:
+      return { background: 'white' };
+  }
+};
+
+const getTdText = (tdData: number) => {
+  switch (tdData) {
+    case CODE.NORMAL:
+      return '';
+    case CODE.MINE:
+      return 'X';
+    default:
+      return '';
+  }
+};
+
 function MineTd({ tdData }: IProps) {
-  return <td>{tdData}</td>;
+  return <td style={getTdStyle(tdData)}>{getTdText(tdData)}</td>;
 }
 
 export default MineTd;
